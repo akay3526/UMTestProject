e55f289a76f43a976de5450556a24e4ec43fec75
akay <72069261+akay3526@users.noreply.github.com>
2023-05-01 14:59:11 +0100

    Update

---
+++ Drivers/DriverClass.cs
@@ -0,0 +1,28 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Remote;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+
+namespace UniversalMusicTestTaskProject.Drivers
+{
+    public class SeleniumDriver
+    {
+        private IWebDriver driver;
+        private readonly ScenarioContext scenarioContext;
+
+        public SeleniumDriver(ScenarioContext scenarioContext) => scenarioContext = scenarioContext;
+
+        public IWebDriver Setup()
+        {
+
+            driver = new OpenQA.Selenium.Firefox.FirefoxDriver();
+            driver.Manage().Window.Maximize();
+            return driver;
+        }
+    }
+}
